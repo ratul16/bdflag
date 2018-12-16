eb3c42a0f02e43fa3e60bd36f386874992b1c277
Md. Hasibul Alam Ratul <haratul@gmail.com>
2018-12-16 17:02:29 +0600

    color fixed

--- flag.js
+++ flag.js
@@ -4,10 +4,10 @@ canvas.width = window.innerWidth *0.6; // creating a height and width
 canvas.height = window.innerHeight * 0.6;
 var cont = canvas.getContext('2d');
 
-cont.fillStyle = '#388E3C';
+cont.fillStyle = '#006747';
 cont.fillRect(0, 0, 720, 420); // fillRect(x-coordinate, y-coordinate, width, height)
 
 cont.beginPath();
 cont.arc(350, 200, 110, 0 * Math.PI, 2 * Math.PI); // arc(x,y,round,sAngle,eAngle, counterClockWise)
-cont.fillStyle = '#E53935';
+cont.fillStyle = '#DA291C';
 cont.fill();
