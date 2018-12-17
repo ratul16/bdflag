a46648e8f89a5bd8fcf9ff0685a095c92fad3a96
Md. Hasibul Alam Ratul <haratul@gmail.com>
2018-12-17 23:32:54 +0600

    size ficed

--- flag.js
+++ flag.js
@@ -1,13 +1,13 @@
 var canvas = document.getElementById('bdflag'); // create a canvas variable
 
-canvas.width = window.innerWidth *0.6; // creating a height and width
-canvas.height = window.innerHeight * 0.6;
+canvas.width = window.innerWidth; // creating a height and width
+canvas.height = window.innerHeight;
 var cont = canvas.getContext('2d');
 
 cont.fillStyle = '#006747';
-cont.fillRect(0, 0, 720, 420); // fillRect(x-coordinate, y-coordinate, width, height)
+cont.fillRect(0, 0, 1000, 600); // fillRect(x-coordinate, y-coordinate, width, height)
 
 cont.beginPath();
-cont.arc(350, 200, 110, 0 * Math.PI, 2 * Math.PI); // arc(x,y,round,sAngle,eAngle, counterClockWise)
+cont.arc(450, 300, 200, 0, 2 * Math.PI); // arc(x,y,round,sAngle,eAngle, counterClockWise)
 cont.fillStyle = '#DA291C';
 cont.fill();
